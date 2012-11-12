ce567003f9fc780fcc0c0f7294950f0b784048e0
Jan <jalobe@inf.fu-berlin.de>
2012-11-12 16:34:54 +0100

    fixed bug and changed threshold computation
    threshold.m is not needed anymore

--- auf32.m
+++ auf32.m
@@ -2,19 +2,19 @@ clear all;
 close all;
 clc;
 
-% Daten aus Dateien einlesen
+# Daten aus Dateien einlesen
 testfile='pendigits-testing.txt';
 trainfile='pendigits-training.txt';
 X_train=dlmread(trainfile);
 X_test=dlmread(testfile);
 
-% Matrix der Erkennungsraten
+# Matrix der Erkennungsraten
 ER = zeros(10);
 
-% Container für Cluster,deren Mittelpunkte und Kovarianzmatrizen
+# Container für Cluster,deren Mittelpunkte und Kovarianzmatrizen
 Data = cell(10,3);
 
-% Finde Cluster, Means und Kovarianzmatrizen
+# Finde Cluster, Means und Kovarianzmatrizen
 for i=0:9
     idx=find(X_train(:,end)==i);
     Data{i+1,1} = X_train(idx,1:16);
@@ -28,27 +28,33 @@ for i=0:8
         u2 = Data{j+1,2};
         E1 = Data{i+1,3};
         E2 = Data{j+1,3};
-        % Finde Projektionsrichtung w
+        # Finde Projektionsrichtung w
         w = (u1 - u2)/(E1 + E2);
-        % Normalisiere w
+        # Normalisiere w
         w = w/(norm(w));
-        % Projiziere Punkte auf w und berechne Mittelwert und Varianz
-        mean1 = mean(Data{i+1,1} * w');
-        mean2 = mean(Data{j+1,1} * w');
-        var1 = mean((Data{i+1,1} * w' .- mean1).^2);
-        var2 = mean((Data{j+1,1} * w' .- mean2).^2);
-        % Berechne Schwellenwert für Klassentrennung
-        t = threshold(mean1,mean2,var1,var2);
+        # Projiziere Punkte auf w und berechne Mittelwert und Varianz
+        proj1 = Data{i+1,1} * w';
+        proj2 = Data{j+1,1} * w';
+        mean1 = mean(proj1);
+        mean2 = mean(proj2);
+        var1 = mean((proj1 - mean1).^2);
+        var2 = mean((proj2 - mean2).^2);
+        # Klassifiziere Punkte aus Testmenge
         hits = 0;
         for k=1:size(X_test,1)
-            proj = w'*X_test(k,1:16);
-            if(proj < t && mean1 < mean2 || proj > t && mean1 > mean2)
-                class = i;
+            x = X_test(k,1:16)*w';
+            p1 = 1/(sqrt(2*pi*var1))*e^(-0.5*((x-mean1)^2)/var1);
+            p2 = 1/(sqrt(2*pi*var2))*e^(-0.5*((x-mean2)^2)/var2);
+            if(p1>p2)
+#printf("check\n");
+                    belief = i;
             else
-                class = j;
+                    belief = j;
             endif
-            if(class == X_test(k,end))
-                hits = hits + 1;
+            actual = X_test(k,end);
+            if(actual == belief)
+#printf("%d %d %d\n",belief,i,j);
+                    hits = hits + 1;
             endif
         end
         rate = hits*100/size(X_test,1);
@@ -56,5 +62,5 @@ for i=0:8
     end
 end
 
-% ER ausgeben
+# ER ausgeben
 ER
