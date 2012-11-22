3d0c3df79e93f38af3100caa0e3d60adc68b389e
Jan <jalobe@inf.fu-berlin.de>
2012-11-22 13:24:44 +0100

    Final Version + Plots

--- auf42.m
+++ auf42.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 clc;
 
-# Daten einlesen
+% Daten einlesen
 file = dlmread('prostatecancer.txt');
 
 X_test = file(find(file(:,end)==0),:);
@@ -19,17 +19,43 @@ X_train(:,1) = 1;
 
 X_all = [X_test; X_train];
 
-      
+%%%%%%%%%%%%
+% Aufgabe 1
+%%%%%%%%%%%%
+
+% X * a = y
+% a berechnen
+a = ((X_train'*X_train)^-1)*X_train'*y_train;
+% Werte vorhersagen
+y_predict = X_test * a;
+% Quadratische Abweichung berechnen und ausgeben
+error = y_test - y_predict;
+q = error'*error
+
+
+
+
+%%%%%%%%%%%%
+% Aufgabe 2
+%%%%%%%%%%%%      
+
 for i=1:8
       subsets = nchoosek(1:8,i);
       for j=1:size(subsets,1)
             X = X_train(:,[1,subsets(j,:).+1]);
             a = ((X'*X)^-1)*X'*y_train;
-                  
-                  y_predict = X_all(:,[1,subsets(j,:).+1]) * a;
+            y_predict = X_all(:,[1,subsets(j,:).+1]) * a;
             error = [y_test;y_train] - y_predict;
             q = error'*error;
+            qs(j) = q;
+            figure(1);
             plot(i,q,'xr');
             hold on;
       end
+      figure(2);
+      plot(i,min(qs),'xr');
+      hold on;
 end
+
+saveas(1,'plot1.png');
+saveas(2,'plot2.png');
