611af4491b40f1b42382a9f446d0779634eaaf7b
Jan <jalobe@inf.fu-berlin.de>
2012-12-06 12:55:25 +0100

    Kantenerkennung auf Testbild geadded

--- auf63.m
+++ auf63.m
@@ -1,3 +1,8 @@
+clear all;
+close all;
+clc;
+
+% Finde alle möglichen Inputs
 A = 0;
 B = 1;
 s = 2;
@@ -12,13 +17,12 @@ datasize = 512;
 
 X = [A;B];
 
+% Finde Lables
 y = zeros(datasize,1);
 y(find(X(:,5)==0)) = 1;
 y(1) = 0;
 
 X = [ones(datasize,1),X];
-
-
 
 % Lernen des Gewichtsvektors w
 w = rand(10,1);
@@ -41,3 +45,26 @@ while(any(y - y_predict))
 	y_predict = ones(datasize,1);
 	y_predict(idx,:) = 0;		
 endwhile
+
+
+%a circle
+[xs,ys]=meshgrid(-100:100);
+I=zeros(size(xs));
+I(sqrt(xs.^2+ys.^2)<(0.3*size(xs,1)))=1;
+ 
+%display
+figure;
+imagesc(I); colormap('gray'); axis equal off;
+
+for i=1:199
+	for j=1:199
+		v = [1,I(i,j:j+2),I(i+1,j:j+2),I(i+2,j:j+2)];
+		v = v * w;
+		if(v<0)
+		kante = 0;
+		else
+		kante = 1;
+		endif 	
+		E(i,j) = kante;
+	end
+end
